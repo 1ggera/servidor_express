3bd4c9d49b3659842f869eba1efade7a756be425
Gerardo Garcia <39609442+Linggera@users.noreply.github.com>
2019-04-24 14:05:27 -0300

    Add files via upload

---
+++ servidor.js
@@ -0,0 +1,18 @@
+//VAMOSPORTODO
+var express = require("express");
+var aplicacion = express()
+
+aplicacion.get("/", inicio);
+aplicacion.get("/cursos", cursos);
+
+function inicio(peticion, resultado)
+{
+    resultado.send("Este es el <strong>home</strong> de LINGGERATOS");
+}
+
+function cursos(peticion, resultado)
+{
+    resultado.send("Estos son los <strong>cursos</strong>")
+}
+
+aplicacion.listen(8989);
